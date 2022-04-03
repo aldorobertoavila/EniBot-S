55810f885bf17b49ae231d663d9dadf049af0d39
Aldo Avila <45251738+aldorobertoavila@users.noreply.github.com>
2022-04-03 09:34:58 -0700

    Add: Console & Controls Frame

--- src/hmi/log.py
+++ src/hmi/log.py
@@ -1,8 +1,14 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+from tkinter import scrolledtext
 from datetime import datetime
 
 import logging
-# import os
+import queue
+import serial
 import time
+
 
 class Timer:
     def __init__(self, ref):
@@ -10,31 +16,147 @@ class Timer:
 
     def millis(self):
         return get_current_millis() - self.ref
-    
+
+
 def get_current_millis():
     return round(time.time() * 1000)
 
-# def make_run_folder(folder):
-#    today = datetime.today()
-#    date = today.strftime("%Y-%m-%d_%H.%M.%S")
-#    time = today.strftime("%H-%M%-S")
-#    folder = f"debug/{date}-{time}"
-#    path = os.path.join(os.getcwd(), folder, f"{date}-{time}")
-#    
-#    os.mkdir(path)
-#    return path.join()
 
 def get_filename():
     return datetime.today().strftime("%Y-%m-%d-%H.%M.%S")
 
-def get_logger(folder, filename): 
-    logging.basicConfig(filename=f"{folder}/{filename}.log", format="%(asctime)s %(levelname)s %(message)s", filemode="w") 
-    logger = logging.getLogger() 
+
+def get_logger(folder, filename):
+    logging.basicConfig(filename=f"{folder}/{filename}.log",
+                        format="%(asctime)s %(levelname)s %(message)s", filemode="w")
+    logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
     return logger
- 
-# logger.debug("Harmless debug Message")
-# logger.info("Just an information")
-# logger.warning("Its a Warning")
-# logger.error("Did you try to divide by zero")
-# logger.critical("Internet is down")
+
+
+class Arduino():
+    def __init__(self, device):
+        self.port = device
+
+    def sendCommand(self, command):
+        self.port.write(command)
+        echo = self.read_string()
+        res = self.read_string()
+        print(echo)
+        print(res)
+        return [echo, res]
+
+    def read_string(self):
+        return self.port.readline().decode("utf-8")
+
+
+class QueueHandler(logging.Handler):
+    def __init__(self, log_queue):
+        super().__init__()
+        self.log_queue = log_queue
+
+    def emit(self, record):
+        self.log_queue.put(record)
+
+
+class ConsoleFrame(tk.Frame):
+    def __init__(self, parent, logger, **kw):
+        tk.Frame.__init__(self, parent, **kw)
+        self.logger = logger
+
+        self.vertical_pane = ttk.PanedWindow(self, orient=tk.VERTICAL)
+        self.vertical_pane.grid(row=0, column=0, sticky="nsew")
+        self.horizontal_pane = ttk.PanedWindow(
+            self.vertical_pane, orient=tk.HORIZONTAL)
+        self.vertical_pane.add(self.horizontal_pane)
+        self.form_frame = ttk.Labelframe(self.horizontal_pane, text="MyForm")
+        self.form_frame.columnconfigure(1, weight=1)
+        self.horizontal_pane.add(self.form_frame, weight=1)
+        self.console_frame = ttk.Labelframe(
+            self.horizontal_pane, text="Console")
+        self.console_frame.columnconfigure(0, weight=1)
+        self.console_frame.rowconfigure(0, weight=1)
+        self.horizontal_pane.add(self.console_frame, weight=1)
+        self.third_frame = ttk.Labelframe(
+            self.vertical_pane, text="Third Frame")
+        self.vertical_pane.add(self.third_frame, weight=1)
+
+        self.form = FormUi(self.form_frame, self.logger)
+        self.console = ConsoleUi(self.console_frame, self.logger)
+
+
+class ConsoleUi:
+    def __init__(self, frame, logger):
+        self.frame = frame
+        self.logger = logger
+
+        self.scrolled_text = scrolledtext.ScrolledText(
+            frame, state='disabled', height=12)
+        self.scrolled_text.grid(
+            row=0, column=0, sticky=(tk.N, tk.S, tk.W, tk.E))
+        self.scrolled_text.configure(font='TkFixedFont')
+        self.scrolled_text.tag_config('INFO', foreground='black')
+        self.scrolled_text.tag_config('DEBUG', foreground='gray')
+        self.scrolled_text.tag_config('WARNING', foreground='orange')
+        self.scrolled_text.tag_config('ERROR', foreground='red')
+        self.scrolled_text.tag_config(
+            'CRITICAL', foreground='red', underline=1)
+
+        self.log_queue = queue.Queue()
+        self.queue_handler = QueueHandler(self.log_queue)
+        formatter = logging.Formatter('%(asctime)s: %(message)s')
+
+        self.queue_handler.setFormatter(formatter)
+        self.logger.addHandler(self.queue_handler)
+
+        self.frame.after(100, self.poll_log_queue)
+
+    def display(self, record):
+        msg = self.queue_handler.format(record)
+        self.scrolled_text.configure(state='normal')
+        self.scrolled_text.insert(tk.END, msg + '\n', record.levelname)
+        self.scrolled_text.configure(state='disabled')
+
+        self.scrolled_text.yview(tk.END)
+
+    def poll_log_queue(self):
+        while True:
+            try:
+                record = self.log_queue.get(block=False)
+            except queue.Empty:
+                break
+            else:
+                self.display(record)
+        self.frame.after(100, self.poll_log_queue)
+
+
+class FormUi:
+    def __init__(self, frame, logger):
+        self.logger = logger
+        self.frame = frame
+        values = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
+        self.level = tk.StringVar()
+        ttk.Label(self.frame, text='Level:').grid(column=0, row=0, sticky=tk.W)
+        self.combobox = ttk.Combobox(
+            self.frame,
+            textvariable=self.level,
+            width=25,
+            state='readonly',
+            values=values
+        )
+        self.combobox.current(0)
+        self.combobox.grid(column=1, row=0, sticky=(tk.W, tk.E))
+        # Create a text field to enter a message
+        self.message = tk.StringVar()
+        ttk.Label(self.frame, text='Message:').grid(
+            column=0, row=1, sticky=tk.W)
+        ttk.Entry(self.frame, textvariable=self.message, width=25).grid(
+            column=1, row=1, sticky=(tk.W, tk.E))
+        # Add a button to log the message
+        self.button = ttk.Button(
+            self.frame, text='Submit', command=self.submit_message)
+        self.button.grid(column=1, row=2, sticky=tk.W)
+
+    def submit_message(self):
+        lvl = getattr(logging, self.level.get())
+        self.logger.log(lvl, self.message.get())
--- src/hmi/main.py
+++ src/hmi/main.py
@@ -1,22 +1,42 @@
 import tkinter as tk
-import tkinter.ttk as tkk
+import tkinter.ttk as ttk
 
 import os
 
 from PIL import ImageTk, Image
 from tkinter import StringVar, messagebox
 
-LOGO = os.path.join(os.getcwd(), '../assets/img/logo.png')
-SCHEM = os.path.join(os.getcwd(), '../assets/img/schematic.png')
+import log
+
+CWD = os.getcwd()
+IMG_PATH = os.path.join(CWD, '../assets/img')
+RUN_PATH = os.path.join(CWD, '../../run')
+
+DEBUG_PATH = os.path.join(RUN_PATH, 'logs')
+DATAGEN_PATH = os.path.join(RUN_PATH, 'datagen')
+CSV_PATH = os.path.join(DATAGEN_PATH, 'csv')
+EXCEL_PATH = os.path.join(DATAGEN_PATH, 'excel')
+
+LED_OFF_PATH = os.path.join(IMG_PATH, 'led_off.png')
+LED_ON_PATH = os.path.join(IMG_PATH, 'led_on.png')
+
+LOGO_PATH = os.path.join(IMG_PATH, 'logo.png')
+SCHEM_PATH = os.path.join(IMG_PATH, 'schematic.png')
 
 FRAME_PADX = 15
 FRAME_PADY = 15
 # assets/img/logo.png
-LOGO_WIDTH = 175
+LOGO_WIDTH = 195
 LOGO_HEIGHT = 100
 # assets/img/schematic.png
 SCHEM_WIDTH = 380
 SCHEM_HEIGHT = 300
+# assets/img/led_off.png
+LED_WIDTH = 25
+LED_HEIGHT = 25
+
+CONTROL_HEIGHT = 3
+CONTROL_WIDTH = 6
 
 
 def get_photo_image(path, size):
@@ -24,7 +44,7 @@ def get_photo_image(path, size):
     return ImageTk.PhotoImage(image)
 
 
-class ConfigFrame(tk.Frame):
+class SettingsFrame(tk.Frame):
     def __init__(self, parent, **kw):
         tk.Frame.__init__(self, parent, **kw)
 
@@ -83,6 +103,9 @@ class ConfigFrame(tk.Frame):
         self.rightward_velocity_var.trace("w", self.rightward_limit_byte)
         self.reverse_velocity_var.trace("w", self.reverse_limit_byte)
 
+        self.submit_button = tk.Button(self, text='Submit')
+        self.submit_button.pack()
+
     def forward_limit_byte(self, *args):
         val = self.forward_velocity_var.get()
         if len(val) > 3:
@@ -104,6 +127,75 @@ class ConfigFrame(tk.Frame):
             self.reverse_velocity_var.set(val[:3])
 
 
+class ControlsFrame(tk.Frame):
+    def __init__(self, parent, **kw):
+        tk.Frame.__init__(self, parent, **kw)
+
+        self.header_lb = tk.Label(
+            self, font='Roboto 32', text='Controls')
+        self.header_lb.pack(anchor='center', expand=True, side='top')
+
+        self.directions_frame = tk.Frame(self)
+        self.directions_frame.pack()
+
+        self.forward_button = tk.Button(
+            self.directions_frame, height=CONTROL_HEIGHT, width=CONTROL_WIDTH, text="f")
+        self.forward_button.grid(row=0, column=1)
+
+        self.leftward_button = tk.Button(
+            self.directions_frame, height=CONTROL_HEIGHT, width=CONTROL_WIDTH, text="l")
+        self.leftward_button.grid(row=1, column=0)
+
+        self.rightward_button = tk.Button(
+            self.directions_frame, height=CONTROL_HEIGHT, width=CONTROL_WIDTH, text="r")
+        self.rightward_button.grid(row=1, column=2)
+
+        self.backward_button = tk.Button(
+            self.directions_frame, height=CONTROL_HEIGHT, width=CONTROL_WIDTH, text="b")
+        self.backward_button.grid(row=2, column=1)
+
+        self.directions_frame.grid_rowconfigure(1, weight=1)
+        self.directions_frame.grid_columnconfigure(1, weight=1)
+
+
+class GraphFrame(tk.Frame):
+    def __init__(self, parent, **kw):
+        tk.Frame.__init__(self, parent, **kw)
+
+        self.header_lb = tk.Label(
+            self, font='Roboto 32', text='Sensors')
+        self.header_lb.grid(row=1, column=1, sticky="ew")
+
+        self.sensors_frame = tk.Frame(self)
+        self.sensors_frame.grid(row=2, column=1, sticky="w")
+        
+        self.led_off_photo = get_photo_image(LED_OFF_PATH, (LED_WIDTH, LED_HEIGHT))
+        self.led_on_photo = get_photo_image(LED_ON_PATH, (LED_WIDTH, LED_HEIGHT))
+
+        self.infrared1 = tk.Label(self.sensors_frame, height=LED_HEIGHT, width=LED_WIDTH,
+                             padx=FRAME_PADX, pady=FRAME_PADY, image=self.led_off_photo)
+        self.infrared1.pack(side="left", fill=None, expand=False)
+        
+        self.infrared2 = tk.Label(self.sensors_frame, height=LED_HEIGHT, width=LED_WIDTH,
+                             padx=FRAME_PADX, pady=FRAME_PADY, image=self.led_off_photo)
+        self.infrared2.pack(side="left", fill=None, expand=False)
+        
+        self.infrared3 = tk.Label(self.sensors_frame, height=LED_HEIGHT, width=LED_WIDTH,
+                             padx=FRAME_PADX, pady=FRAME_PADY, image=self.led_off_photo)
+        self.infrared3.pack(side="left", fill=None, expand=False)
+
+        self.ultrasonic1_button = tk.Button(self.sensors_frame, text='U1')
+        self.ultrasonic1_button.pack(side="left", fill=None, expand=False)
+
+        self.ultrasonic2_button = tk.Button(self.sensors_frame, text='U2')
+        self.ultrasonic2_button.pack(side="left", fill=None, expand=False)
+
+        self.ultrasonic3_button = tk.Button(self.sensors_frame, text='U3')
+        self.ultrasonic3_button.pack(side="left", fill=None, expand=False)
+
+        self.grid_columnconfigure(1, weight=1)
+
+
 class HMI(tk.Tk):
     def __init__(self, parent, **kw):
         tk.Tk.__init__(self, parent, **kw)
@@ -114,7 +206,7 @@ class HMI(tk.Tk):
         self.header_frame = tk.Frame(
             self, padx=FRAME_PADX, pady=FRAME_PADY)
         self.header_frame.pack(fill=tk.X, side=tk.TOP)
-        self.logo_photo = get_photo_image(LOGO, (LOGO_WIDTH, LOGO_HEIGHT))
+        self.logo_photo = get_photo_image(LOGO_PATH, (LOGO_WIDTH, LOGO_HEIGHT))
 
         self.logo = tk.Label(self.header_frame, width=LOGO_WIDTH, height=LOGO_HEIGHT,
                              padx=FRAME_PADX, pady=FRAME_PADY, image=self.logo_photo)
@@ -128,18 +220,33 @@ class HMI(tk.Tk):
             self, padx=FRAME_PADX, pady=FRAME_PADY, bg='#64a0dd')
         self.body_frame.pack(fill=tk.X, side=tk.TOP)
 
-        self.config_frame = ConfigFrame(
-            self.body_frame, height=400, width=380, bg='#dde061')
-        self.config_frame.pack(side=tk.LEFT)
+        self.settings_frame = SettingsFrame(
+            self.body_frame, padx=FRAME_PADX, pady=FRAME_PADY, height=400, width=380, bg='#dde061')
+        self.settings_frame.pack(side=tk.LEFT)
+
+        self.controls_frame = ControlsFrame(
+            self.body_frame, padx=FRAME_PADX, pady=FRAME_PADY, height=400, width=380, bg='#3d59a5')
+        self.controls_frame.pack(side=tk.LEFT)
+        
+        self.graph_frame = GraphFrame(
+            self.body_frame, padx=FRAME_PADX, pady=FRAME_PADY, height=400, width=380, bg='#bb6219')
+        self.graph_frame.pack(side=tk.BOTTOM)
 
         self.schem_frame = tk.Frame(self.body_frame, height=200, width=380)
         self.schem_frame.pack(fill=tk.Y, side=tk.RIGHT)
 
         self.photo = get_photo_image(
-            SCHEM, (SCHEM_WIDTH + 35, SCHEM_HEIGHT + 35))
+            SCHEM_PATH, (SCHEM_WIDTH + 35, SCHEM_HEIGHT + 35))
         self.logo = tk.Label(self.schem_frame, width=SCHEM_WIDTH, height=SCHEM_HEIGHT,
                              padx=FRAME_PADX, pady=FRAME_PADY, image=self.photo)
         self.logo.pack(fill=tk.X, side=tk.TOP)
+
+        self.filename = log.get_filename()
+        self.logger = log.get_logger(DEBUG_PATH, self.filename)
+
+        self.debug_frame = log.ConsoleFrame(
+            self, self.logger, padx=FRAME_PADX, pady=FRAME_PADY, bg='#34608b')
+        self.debug_frame.pack(fill=tk.X, side=tk.TOP)
 
 
 if __name__ == '__main__':
